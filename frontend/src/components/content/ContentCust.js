9ccf141b19b9e455068dd7db82b0be454cc9ccfb
david-develop <924@holbertonschool.com>
2020-03-04 17:39:44 -0500

    [Add] axios ready

--- frontend/src/components/content/ContentCust.js
+++ frontend/src/components/content/ContentCust.js
@@ -1,59 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import ReadMore from '../readmore/ReadMore';
 import StickyBar from '../stickybar/StickyBar';
 import CarouselPic from '../carouselpic/CarouselPic';
 import './ContentCust.scss';
+import axios from 'axios';
 
-const ContentCust = () =>
-  <div className="content-container">
-    <Container fluid>
-      <Row>
-        <Col sm={8} id="main-content-container">
-          <div className="container">
-            <div className="row">
-              {/* First Card */}
-              <div className="card mb-3 card-cont-cust" id="card-main-info">
-                <div className="row no-gutters">
-                  <div className="col-md-3">
-                    <CarouselPic />
-                  </div>
-                  <div className="col-md-9">
-                    <div className="card-body">
-                      <h5 className="card-title">Las Pulgas</h5>
-                      <h6 className="user-text"> Offered by </h6>
-                      <ReadMore />
+class ContentCust extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      coffeName: '',
+    };
+  }
+
+  componentDidMount () {
+    axios('https://swapi.co/api/planets/1/')
+    .then(result => this.setState({coffeName: result.data.name}))
+    .catch(error => error );
+  }
+
+  render () {
+    return (
+      <div className="content-container">
+        <Container fluid>
+          <Row>
+            <Col sm={8} id="main-content-container">
+              <div className="container">
+                <div className="row">
+                  {/* First Card */}
+                  <div className="card mb-3 card-cont-cust" id="card-main-info">
+                    <div className="row no-gutters">
+                      <div className="col-md-3">
+                        <CarouselPic />
+                      </div>
+                      <div className="col-md-9">
+                        <div className="card-body">
+                          <h5 className="card-title">{this.state.coffeName}</h5>
+                          <h6 className="user-text"> Offered by </h6>
+                          <ReadMore />
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              {/* End First Card */}
-              {/* Second Card */}
-              <div className="card mb-3 card-cont-cust" id="card-sub-info">
-                <div className="row no-gutters">
-                  <div className="col-md-3">
-                    <h5 className="card-title" id="specs">General Information</h5>
-                  </div>
-                  <div className="col-md-9">
-                    <div className="card-body">
-                      <p className="card-text">Variety: Castillo, Colombia.</p>
-                      <p className="card-text">Procesing: washed.</p>
-                      <p className="card-text">Crop year: 2020.</p>
-                      <p className="card-text">Couping score: 87.</p>
+                  {/* End First Card */}
+                  {/* Second Card */}
+                  <div className="card mb-3 card-cont-cust" id="card-sub-info">
+                    <div className="row no-gutters">
+                      <div className="col-md-3">
+                        <h5 className="card-title" id="specs">General Information</h5>
+                      </div>
+                      <div className="col-md-9">
+                        <div className="card-body">
+                          <p className="card-text">Variety: Castillo, Colombia.</p>
+                          <p className="card-text">Procesing: washed.</p>
+                          <p className="card-text">Crop year: 2020.</p>
+                          <p className="card-text">Couping score: 87.</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
+                  {/* End Second Card */}
                 </div>
               </div>
-              {/* End Second Card */}
-            </div>
-          </div>
-        </Col>
-        <Col sm={4}>
-        <StickyBar />
-        </Col>
-      </Row>
-    </Container>
-  </div>
+            </Col>
+            <Col sm={4}>
+            <StickyBar />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      )
+    }
+}
 
 export default ContentCust;
