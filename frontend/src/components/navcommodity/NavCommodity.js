7c6d4ac61f92662ea8cdedec5f68d4d95967e8a6
edward0rtiz <931@holbertonschool.com>
2020-03-05 16:58:14 -0500

    [feature] add nav-commodity component

---
+++ frontend/src/components/navcommodity/NavCommodity.js
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import './NavCommodity.scss';
+import { Nav } from 'react-bootstrap';
+
+
+const NavCommodity  = () => {
+        return (
+            <div id="nav-c">
+                <Nav justify variant="tabs" defaultActiveKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home">Cotton</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">Cocoa</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">Avocado</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-3">Sugar Cane</Nav.Link>
+                </Nav.Item>
+                </Nav>    
+            </div>
+    )
+}
+
+export default NavCommodity;
