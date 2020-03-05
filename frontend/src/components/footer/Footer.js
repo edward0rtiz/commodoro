3e5cb97fc82501b8e1fc30daf8062298a92e1430
edward0rtiz <931@holbertonschool.com>
2020-03-05 17:01:26 -0500

    [update] remove shadow at nav.link and modify size font of footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -8,38 +8,38 @@ const Footer = () => {
                 <div className="row">
                     {/* Column 1 */}
                     <div className="col">
-                        <h4>Get to know Us</h4>
+                        <h5s>Get to know Us</h5s>
                         <ul className="list-unstyled">
-                            <h6>
+                            <p>
                             <li>About Commodoro</li>
                             <li>Contact</li>
                             <li>Team</li>
                             <li>Careers</li>
-                            </h6>
+                            </p>
                         </ul>
                     </div>
                     {/* Column 2 */}
                     <div className="col">
-                            <h4>Why Commodoro</h4>
+                            <h5s>Why Commodoro</h5s>
                             <ul className="list-unstyled">
-                                <h6>
+                                <p>
                                 <li>What is a descentralized market</li>
                                 <li>How to buy as a consumer</li>
                                 <li>How to sell as a seller</li>
                                 <li>About real-time transactions</li>
                                 <li>About market analytics</li>
-                                </h6>
+                                </p>
                             </ul>
                     </div>
                     {/* Column 3 */}
                     <div className="col">
-                            <h4>Commodoro social</h4>
+                            <h5s>Commodoro social</h5s>
                             <ul className="list-unstyled">
-                                <h6>
+                                <p>
                                 <li>LinkedIn</li>
                                 <li>Twitter</li>
                                 <li>Instagram</li>
-                                </h6>
+                                </p>
                             </ul>
                     </div>
                 </div>
