6b3337918725309712083943614fb7a27f44422d
david-develop <924@holbertonschool.com>
2020-03-07 17:56:38 -0500

    [Add] businex template

---
+++ frontend-v2/businex/src/components/Header/Logo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import logo from '../../assets/img/commodoro_logo.png'
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-area">
+                <a href="/"><img src={logo} alt="commodoro-Logo" /></a>
+            </div>
+        );
+    }
+}
+
+export default Logo;
