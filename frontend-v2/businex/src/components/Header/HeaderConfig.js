e337c743b19f5193144a64cd7d40f189c53dc8d6
david-develop <924@holbertonschool.com>
2020-03-09 14:24:10 -0500

    [Fix] header style

--- frontend-v2/businex/src/components/Header/HeaderConfig.js
+++ frontend-v2/businex/src/components/Header/HeaderConfig.js
@@ -13,9 +13,9 @@ function HeaderConfig(props) {
     }
 
     return (
-        <div className="header-action mt-lg-3 text-right">
+        <div className="header-action mt-lg-1 text-right">
             {/* <a href="tel:00199823568658" className="tel-no">+998 23568 658</a> */}
-            <button onClick={LoginRegHandler} className="btn-cog"><i className="fa fa-cog"/></button>
+            <button onClick={LoginRegHandler} className="btn-user-circle"><i className="fa fa-user-circle"/></button>
             {/* <button onClick={MobileMenuHandler} className="btn-menu d-lg-none"><i className="fa fa-bars"/></button> */}
         </div>
     );
