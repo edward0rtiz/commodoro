c6effd3bcb60901005f4ed5f8194c23f62f0f89f
david-develop <924@holbertonschool.com>
2020-03-09 19:17:45 -0500

    [Update] header and sticky bar styles

--- frontend-v2/businex/src/components/StickyBar/index.js
+++ frontend-v2/businex/src/components/StickyBar/index.js
@@ -44,10 +44,10 @@ const StickyBar = (props) => {
                             </Container>
                             </Card.Text>
                             <Button variant="link" class="text-right">Add to cart</Button>
-                            <Button variant="success" class="btn-flat" size="md" block active>
+                            <Button className="order-button" variant="success" class="btn-flat" size="md" block active>
                                 Order Sample
                             </Button>
-                            <Button variant="secondary" class="btn-flat" size="md" block active>
+                            <Button className="order-button" variant="secondary" class="btn-flat" size="md" block active>
                                 Contact Seller
                             </Button>
                         </Card.Body>
