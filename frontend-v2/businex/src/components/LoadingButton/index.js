888c48e47f1a5997abea4de8cc7c376824fed198
edward0rtiz <931@holbertonschool.com>
2020-03-13 09:07:03 -0500

    [update] remove render module no needed

--- frontend-v2/businex/src/components/LoadingButton/index.js
+++ frontend-v2/businex/src/components/LoadingButton/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, render} from 'react';
+import React, {useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import "./LoadingButton.scss"
 
