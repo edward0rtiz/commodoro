6841695bbf63a88e67b79db2c4bee654117e103b
edward0rtiz <931@holbertonschool.com>
2020-03-13 08:36:36 -0500

    [feature] loading button component

---
+++ frontend-v2/businex/src/components/LoadingButton/index.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect, render} from 'react';
+import Button from 'react-bootstrap/Button';
+import "./LoadingButton.scss"
+
+function simulateNetworkRequest() {
+    return new Promise(resolve => setTimeout(resolve, 500));
+  }
+  
+  function LoadingButton() {
+    const [isLoading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      if (isLoading) {
+        simulateNetworkRequest().then(() => {
+          setLoading(false);
+        });
+      }
+    }, [isLoading]);
+  
+    const handleClick = () => setLoading(true);
+  
+    return (
+      <Button
+        variant="secondary"
+        className="btn-loading main"
+        size="sm"
+        disabled={isLoading}
+        onClick={!isLoading ? handleClick : null}
+        block active
+      >
+        {isLoading ? 'Loading…' : 'Add another'}
+      </Button>
+    );
+}
+
+export default LoadingButton;
