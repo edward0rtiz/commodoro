d6d3382e79931e8713ac968d76e268a62a194649
edward0rtiz <931@holbertonschool.com>
2020-03-14 10:42:24 -0500

    [update] add Onchange method to accept handle change at form

--- frontend-v2/businex/src/components/UI/Input/index.js
+++ frontend-v2/businex/src/components/UI/Input/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const FormInput = ({tag,type,name,placeholder,classes}) => {
+const FormInput = ({tag,type,name,placeholder,classes, value, handleChange}) => {
     return (
         <div className="single-input-item">
             <label>
                 {(()=>{
                     if(tag === 'inputrequired'){
-                        return <input type={type} name={name} placeholder={placeholder} className={classes} required />
+                        return <input type={type} name={name} placeholder={placeholder} className={classes}  value={value} onChange={handleChange} required />
                     }else if(tag === 'input'){
-                        return <input type={type} name={name} placeholder={placeholder} className={classes} />
+                        return <input type={type} name={name} placeholder={placeholder} className={classes} value={value} onChange={handleChange} />
                     }else if(tag === 'textarea'){
-                        return  <textarea name={name} cols="30" rows="7" placeholder={placeholder} className={classes} required/>
+                        return  <textarea name={name} cols="30" rows="7" placeholder={placeholder} className={classes} value={value} onChange={handleChange} required/>
                     }else if(tag === 'button'){
                         return <button className={`btn-outline ${classes}`}>Save your changes</button>
                     }
