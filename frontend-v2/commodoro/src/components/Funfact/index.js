f549e09128625dba2ac0f0c87cb3e4e68a1e4c46
edward0rtiz <931@holbertonschool.com>
2020-03-26 09:26:02 -0500

    [update] remove temp img fun fact

--- frontend-v2/commodoro/src/components/Funfact/index.js
+++ frontend-v2/commodoro/src/components/Funfact/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import FunfactItem from "./funfactItem";
-
-import funfactbg from '../../assets/img/fun-fact-bg.jpg'
 import Funfacts from '../../data/Funfact/funfact'
 
 function Funfact() {
     return (
-        <div className="fun-fact-area sm-top parallax" style={{backgroundImage: `url(${funfactbg})`}}>
+        // <div className="fun-fact-area sm-top parallax" style={{backgroundImage: `url(${funfactbg})`}}>
+        <div className="fun-fact-area sm-top parallax">
             <div className="container">
                 <div className="row mtn-40">
                     {
